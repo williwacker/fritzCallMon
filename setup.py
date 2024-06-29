0cdf28bff3d78707d09490ba1f92cf00b6880cfb
Werner Kuehn <werner_kuehn@gmx.de>
2024-06-29 22:21:07 +0200

    setup.py aktualisieren

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 import setuptools
 import os
 
+def get_install_requires():
+    install_requires = []
+
+    try:
+        import importlib
+    except ImportError:
+        install_requires.append('importlib')
+
+    try:
+        from collections import OrderedDict
+    except ImportError:
+        install_requires.append('ordereddict')
+
+    return install_requires
+
 here = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8', mode='r') as f:
     long_description = f.read().strip()
@@ -10,13 +25,13 @@ setuptools.setup(
     version='0.3.1',
     author='Werner Kühn',
     author_email='willi1wacker@gmx.de',
-    url='http://github.com/williwacker/fritzCallMon',
+    url='https://github.com/williwacker/fritzCallMon',
     description='Monitor incoming and outgoing external calls in the Fritz!Box and do a backward search for the callers name',
     packages=setuptools.find_packages(),
     long_description=long_description,
     keywords='fritz fritzconnection',
     zip_safe=False,
-    install_requires=['fritzconnection>=1.4.0'],
+    install_requires=get_install_requires(),
     setup_requires=['setuptools-git-versioning<1.8.0'],
     python_requires='>=3',
     test_suite='runtests.runtests',
