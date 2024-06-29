468b250842c920b4578def454e0daa6c323d4c65
Werner Kühn <werner_kuehn@gmx.de>
2024-06-29 16:36:28 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,29 @@
-﻿#!/usr/bin/env python
-
-try:
-    from setuptools import find_packages, setup
-    from setuptools.command.test import test
-except ImportError:
-    from ez_setup import use_setuptools
-
-    use_setuptools()
-    from setuptools import setup, find_packages
-    from setuptools.command.test import test
-
+﻿import setuptools
 import os
 
 here = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8', mode='r') as f:
     long_description = f.read().strip()
 
-setup(
+setuptools.setup(
     name='fritzCallMon',
     version='0.3.1',
     author='Werner Kühn',
     author_email='willi1wacker@gmx.de',
     url='http://github.com/williwacker/fritzCallMon',
     description='Monitor incoming and outgoing external calls in the Fritz!Box and do a backward search for the callers name',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     long_description=long_description,
     keywords='fritz fritzconnection',
     zip_safe=False,
-    install_requires=['fritzCallMon',],
+    install_requires=['fritzconnection>=1.4.0'],
+    setup_requires=['setuptools-git-versioning<1.8.0'],
     python_requires='>=3',
     test_suite='runtests.runtests',
     include_package_data=True,
     classifiers=[
-        'Framework :: Python',
+        "Programming Language :: Python :: 3",
         'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
         'Topic :: Software Development',
     ],
